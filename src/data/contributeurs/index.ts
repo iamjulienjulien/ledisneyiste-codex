c0552b64779a1c51dbde522b2495b12c2154d889
Julien Julien <contact@julienjulien.fr>
2026-08-16 00:18:01 +0200

    🗄️ Archives > Faire entrer Ub Iwerks dans le cercle de Mickey > 🐭 Julien

--- src/data/contributeurs/index.ts
+++ src/data/contributeurs/index.ts
@@ -1,7 +1,11 @@
+import ubIwerksJson from "@/data/contributeurs/ub-iwerks.json";
 import waltDisneyJson from "@/data/contributeurs/walt-disney.json";
 import type { FicheContributeurDisney } from "@/types/contributeur";
 
-export const fichesContributeurs = [waltDisneyJson as FicheContributeurDisney];
+export const fichesContributeurs = [
+    waltDisneyJson as FicheContributeurDisney,
+    ubIwerksJson as FicheContributeurDisney,
+];
 
 export function getFicheContributeurBySlug(slug: string) {
     return fichesContributeurs.find((fiche) => fiche.slug === slug);
