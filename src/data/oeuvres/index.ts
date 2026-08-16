0bc4a687a6d7f813b7d71ddba46c2aee5bff8a79
Julien Julien <contact@julienjulien.fr>
2026-08-16 03:36:56 +0200

    🗄️ Archives > Donald prend son envol avec The Wise Little Hen > 🐭 Julien

--- src/data/oeuvres/index.ts
+++ src/data/oeuvres/index.ts
@@ -1,7 +1,11 @@
 import steamboatWillieJson from "@/data/oeuvres/steamboat-willie.json";
+import theWiseLittleHenJson from "@/data/oeuvres/the-wise-little-hen.json";
 import type { FicheOeuvreDisney } from "@/types/oeuvre";
 
-export const fichesOeuvres = [steamboatWillieJson as FicheOeuvreDisney];
+export const fichesOeuvres = [
+    steamboatWillieJson as FicheOeuvreDisney,
+    theWiseLittleHenJson as FicheOeuvreDisney,
+];
 
 export function getFicheOeuvreBySlug(slug: string) {
     return fichesOeuvres.find((fiche) => fiche.slug === slug);
