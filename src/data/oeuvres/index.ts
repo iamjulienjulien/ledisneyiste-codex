90403f11efcdbe7e210a8dcec63a633053058572
Julien Julien <contact@julienjulien.fr>
2026-08-16 03:42:29 +0200

    🗄️ Archives > Remettre Clock Cleaners à l’heure du Codex > 🐭 Julien

--- src/data/oeuvres/index.ts
+++ src/data/oeuvres/index.ts
@@ -1,3 +1,4 @@
+import clockCleanersJson from "@/data/oeuvres/clock-cleaners.json";
 import steamboatWillieJson from "@/data/oeuvres/steamboat-willie.json";
 import theBandConcertJson from "@/data/oeuvres/the-band-concert.json";
 import theWiseLittleHenJson from "@/data/oeuvres/the-wise-little-hen.json";
@@ -7,6 +8,7 @@ export const fichesOeuvres = [
     steamboatWillieJson as FicheOeuvreDisney,
     theWiseLittleHenJson as FicheOeuvreDisney,
     theBandConcertJson as FicheOeuvreDisney,
+    clockCleanersJson as FicheOeuvreDisney,
 ];
 
 export function getFicheOeuvreBySlug(slug: string) {
