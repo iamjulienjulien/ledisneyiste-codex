544fadcc07f708795549387db2b07e05008fcd19
Julien Julien <contact@julienjulien.fr>
2026-08-16 03:57:48 +0200

    🗄️ Archives > Ward Kimball fait dérailler les habitudes du Codex > 🐭 Julien

--- src/data/contributeurs/index.ts
+++ src/data/contributeurs/index.ts
@@ -2,6 +2,7 @@ import ericLarsonJson from "@/data/contributeurs/eric-larson.json";
 import lesClarkJson from "@/data/contributeurs/les-clark.json";
 import miltKahlJson from "@/data/contributeurs/milt-kahl.json";
 import ubIwerksJson from "@/data/contributeurs/ub-iwerks.json";
+import wardKimballJson from "@/data/contributeurs/ward-kimball.json";
 import waltDisneyJson from "@/data/contributeurs/walt-disney.json";
 import wolfgangReithermanJson from "@/data/contributeurs/wolfgang-reitherman.json";
 import type { FicheContributeurDisney } from "@/types/contributeur";
@@ -13,6 +14,7 @@ export const fichesContributeurs = [
     ericLarsonJson as FicheContributeurDisney,
     wolfgangReithermanJson as FicheContributeurDisney,
     miltKahlJson as FicheContributeurDisney,
+    wardKimballJson as FicheContributeurDisney,
 ];
 
 export function getFicheContributeurBySlug(slug: string) {
