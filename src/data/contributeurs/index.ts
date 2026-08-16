9f87ff606556932d9811064edce94f80d28ea8a3
Julien Julien <contact@julienjulien.fr>
2026-08-16 04:00:41 +0200

    🗄️ Archives > Frank Thomas donne une âme au mouvement > 🐭 Julien

--- src/data/contributeurs/index.ts
+++ src/data/contributeurs/index.ts
@@ -1,4 +1,5 @@
 import ericLarsonJson from "@/data/contributeurs/eric-larson.json";
+import frankThomasJson from "@/data/contributeurs/frank-thomas.json";
 import lesClarkJson from "@/data/contributeurs/les-clark.json";
 import miltKahlJson from "@/data/contributeurs/milt-kahl.json";
 import ubIwerksJson from "@/data/contributeurs/ub-iwerks.json";
@@ -15,6 +16,7 @@ export const fichesContributeurs = [
     wolfgangReithermanJson as FicheContributeurDisney,
     miltKahlJson as FicheContributeurDisney,
     wardKimballJson as FicheContributeurDisney,
+    frankThomasJson as FicheContributeurDisney,
 ];
 
 export function getFicheContributeurBySlug(slug: string) {
