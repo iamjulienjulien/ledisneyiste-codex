a3721316c204f03228db8f4d5b611d7cf92790d9
Julien Julien <contact@julienjulien.fr>
2026-08-16 03:40:45 +0200

    🗄️ Archives > Faire entrer The Band Concert en couleurs dans le Codex > 🐭 Julien

--- src/data/oeuvres/index.ts
+++ src/data/oeuvres/index.ts
@@ -1,10 +1,12 @@
 import steamboatWillieJson from "@/data/oeuvres/steamboat-willie.json";
+import theBandConcertJson from "@/data/oeuvres/the-band-concert.json";
 import theWiseLittleHenJson from "@/data/oeuvres/the-wise-little-hen.json";
 import type { FicheOeuvreDisney } from "@/types/oeuvre";
 
 export const fichesOeuvres = [
     steamboatWillieJson as FicheOeuvreDisney,
     theWiseLittleHenJson as FicheOeuvreDisney,
+    theBandConcertJson as FicheOeuvreDisney,
 ];
 
 export function getFicheOeuvreBySlug(slug: string) {
