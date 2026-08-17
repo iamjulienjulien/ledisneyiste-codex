6c28d2a28a8876a1dd03f531de4e857e3a825382
Julien Julien <contact@julienjulien.fr>
2026-08-17 19:33:44 +0200

    🏗️ Décor > Autoriser le nouveau domaine local du Codex > 🐭 Julien

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
     devIndicators: {
         position: "bottom-right",
     },
+    allowedDevOrigins: ["ledisneyiste-codex.test"],
 };
 
 export default nextConfig;
