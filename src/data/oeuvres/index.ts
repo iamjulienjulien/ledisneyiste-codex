bed0dfd539d9654254c3b48ca97ddad498d89847
Julien Julien <contact@julienjulien.fr>
2026-08-18 01:57:36 +0200

    🗄️ Archives > The Gallopin’ Gaucho donne à Mickey son premier rôle > 🐭 Julien

--- src/data/oeuvres/index.ts
+++ src/data/oeuvres/index.ts
@@ -5,6 +5,7 @@ import mrDuckStepsOutJson from "@/data/oeuvres/mr-duck-steps-out.json";
 import planeCrazyJson from "@/data/oeuvres/plane-crazy.json";
 import steamboatWillieJson from "@/data/oeuvres/steamboat-willie.json";
 import theBandConcertJson from "@/data/oeuvres/the-band-concert.json";
+import theGallopinGauchoJson from "@/data/oeuvres/the-gallopin-gaucho.json";
 import theWiseLittleHenJson from "@/data/oeuvres/the-wise-little-hen.json";
 import trolleyTroublesJson from "@/data/oeuvres/trolley-troubles.json";
 import type { FicheOeuvreDisney } from "@/types/oeuvre";
@@ -13,6 +14,7 @@ export const fichesOeuvres = [
     alicesWonderlandJson as FicheOeuvreDisney,
     trolleyTroublesJson as FicheOeuvreDisney,
     steamboatWillieJson as FicheOeuvreDisney,
+    theGallopinGauchoJson as FicheOeuvreDisney,
     planeCrazyJson as FicheOeuvreDisney,
     theWiseLittleHenJson as FicheOeuvreDisney,
     theBandConcertJson as FicheOeuvreDisney,
