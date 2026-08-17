5796f1011dbbdb86dcefbac1e2698d6bcefdebea
Julien Julien <contact@julienjulien.fr>
2026-08-18 01:55:38 +0200

    🗄️ Archives > Plane Crazy fait décoller le premier Mickey > 🐭 Julien

--- src/data/oeuvres/index.ts
+++ src/data/oeuvres/index.ts
@@ -2,6 +2,7 @@ import alicesWonderlandJson from "@/data/oeuvres/alices-wonderland.json";
 import boneTroubleJson from "@/data/oeuvres/bone-trouble.json";
 import clockCleanersJson from "@/data/oeuvres/clock-cleaners.json";
 import mrDuckStepsOutJson from "@/data/oeuvres/mr-duck-steps-out.json";
+import planeCrazyJson from "@/data/oeuvres/plane-crazy.json";
 import steamboatWillieJson from "@/data/oeuvres/steamboat-willie.json";
 import theBandConcertJson from "@/data/oeuvres/the-band-concert.json";
 import theWiseLittleHenJson from "@/data/oeuvres/the-wise-little-hen.json";
@@ -12,6 +13,7 @@ export const fichesOeuvres = [
     alicesWonderlandJson as FicheOeuvreDisney,
     trolleyTroublesJson as FicheOeuvreDisney,
     steamboatWillieJson as FicheOeuvreDisney,
+    planeCrazyJson as FicheOeuvreDisney,
     theWiseLittleHenJson as FicheOeuvreDisney,
     theBandConcertJson as FicheOeuvreDisney,
     clockCleanersJson as FicheOeuvreDisney,
