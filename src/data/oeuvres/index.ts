aad0ad6554232664822591efe1cf1031ea5480cf
Julien Julien <contact@julienjulien.fr>
2026-08-18 02:00:05 +0200

    🗄️ Archives > The Skeleton Dance libère le dessin par la musique > 🐭 Julien

--- src/data/oeuvres/index.ts
+++ src/data/oeuvres/index.ts
@@ -6,6 +6,7 @@ import planeCrazyJson from "@/data/oeuvres/plane-crazy.json";
 import steamboatWillieJson from "@/data/oeuvres/steamboat-willie.json";
 import theBandConcertJson from "@/data/oeuvres/the-band-concert.json";
 import theGallopinGauchoJson from "@/data/oeuvres/the-gallopin-gaucho.json";
+import theSkeletonDanceJson from "@/data/oeuvres/the-skeleton-dance.json";
 import theWiseLittleHenJson from "@/data/oeuvres/the-wise-little-hen.json";
 import trolleyTroublesJson from "@/data/oeuvres/trolley-troubles.json";
 import type { FicheOeuvreDisney } from "@/types/oeuvre";
@@ -16,6 +17,7 @@ export const fichesOeuvres = [
     steamboatWillieJson as FicheOeuvreDisney,
     theGallopinGauchoJson as FicheOeuvreDisney,
     planeCrazyJson as FicheOeuvreDisney,
+    theSkeletonDanceJson as FicheOeuvreDisney,
     theWiseLittleHenJson as FicheOeuvreDisney,
     theBandConcertJson as FicheOeuvreDisney,
     clockCleanersJson as FicheOeuvreDisney,
