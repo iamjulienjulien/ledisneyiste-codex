8c3b0d2afd53155a5efa077b7fa9127106176423
Julien Julien <contact@julienjulien.fr>
2026-08-18 02:41:03 +0200

    🗄️ Archives > Horace Horsecollar met sa polyvalence au service de la troupe > 🐭 Julien

--- src/data/personnages/index.ts
+++ src/data/personnages/index.ts
@@ -2,6 +2,7 @@ import clarabelleCowJson from "@/data/personnages/clarabelle-cow.json";
 import daisyDuckJson from "@/data/personnages/daisy-duck.json";
 import donaldDuckJson from "@/data/personnages/donald-duck.json";
 import dingoJson from "@/data/personnages/dingo.json";
+import horaceHorsecollarJson from "@/data/personnages/horace-horsecollar.json";
 import mickeyMouseJson from "@/data/personnages/mickey-mouse.json";
 import minnieMouseJson from "@/data/personnages/minnie-mouse.json";
 import oswaldLeLapinChanceuxJson from "@/data/personnages/oswald-le-lapin-chanceux.json";
@@ -15,6 +16,7 @@ export const fichesPersonnages = [
     mickeyMouseJson as FichePersonnageDisney,
     minnieMouseJson as FichePersonnageDisney,
     clarabelleCowJson as FichePersonnageDisney,
+    horaceHorsecollarJson as FichePersonnageDisney,
     donaldDuckJson as FichePersonnageDisney,
     dingoJson as FichePersonnageDisney,
     daisyDuckJson as FichePersonnageDisney,
