1a8a6c8f7c3714447398d2aa368e62f4bfe911ea
Julien Julien <contact@julienjulien.fr>
2026-08-28 13:50:12 +0200

    🩹 Raccord > Laisser respirer le générique au bas des pages > 🐭 Julien

--- src/components/codex/CodexFiche/CodexFiche.tsx
+++ src/components/codex/CodexFiche/CodexFiche.tsx
@@ -6,7 +6,7 @@ export function CodexFiche({ family, children }: CodexFicheProps) {
     return (
         <main className={styles.root} data-codex-family={family}>
             {children}
-            <CodexFooter className="mt-16 !px-0" />
+            <CodexFooter className="!px-0" />
         </main>
     );
 }
